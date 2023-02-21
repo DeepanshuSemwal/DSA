e1b26bed452b6b55aba3fa830c0c296bc7ca3be7
Deepanshu <semwaldeepanshu@gmail.com>
2023-02-21 23:45:21 +0530

    6

---
+++ src/duplicates_sortedarray/solution.java
@@ -0,0 +1,17 @@
+package duplicates_sortedarray;
+
+public class solution {
+    public static int sorted(int arr[],int n)
+    {
+        int res=1;
+        for(int i=1;i<n;i++)
+        {
+            if(arr[res-1]!=arr[i])
+            {
+                arr[res]=arr[i];
+                res++;
+            }
+        }
+        return res;
+    }
+}
