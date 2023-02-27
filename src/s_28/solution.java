7623d0dd3c156811e2f473e2385f2292d1afecf5
Deepanshu <semwaldeepanshu@gmail.com>
2023-02-27 22:31:06 +0530

    28

---
+++ src/s_28/solution.java
@@ -0,0 +1,29 @@
+package s_28;
+
+public class solution {
+    public int maxArea(int[] arr) {
+
+        // check the code
+        int n=arr.length;
+        int low=0;
+        int high=n-1;
+        int res=0;
+        while(low<high)
+        {
+            int lh=arr[low];
+            int rh=arr[high];
+            int min_height=Math.min(lh,rh);
+            int distance=high-low;
+            res=Math.max(res,min_height*distance);
+            if(lh<rh)
+            {
+                lh++;
+            }
+            else  if(lh>rh)
+            {
+                rh--;
+            }
+        }
+        return res;
+    }
+}
