8003e76702c6a1de54745f8b9c161a1b5fa29f59
Deepanshu <semwaldeepanshu@gmail.com>
2023-03-10 15:49:35 +0530

    s_124

---
+++ src/s_124/solution.java
@@ -0,0 +1,29 @@
+package s_124;
+
+import java.util.List;
+
+// Definition for singly-linked list.
+  class ListNode {
+      int val;
+      ListNode next;
+      ListNode() {}
+      ListNode(int val) { this.val = val; }
+      ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+  }
+public class solution {
+    public ListNode reverseList(ListNode head) {
+
+        ListNode cur=head;
+        ListNode prev=null;
+        while(cur!=null)
+        {
+            ListNode temp=cur.next;
+            cur.next=prev;
+            prev=cur;
+            cur=temp;
+
+        }
+        return prev;
+
+    }
+}
