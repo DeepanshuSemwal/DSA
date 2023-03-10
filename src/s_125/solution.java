1232978abfcab1eefaaaea1fe83a1b04e59e9b91
Deepanshu <semwaldeepanshu@gmail.com>
2023-03-10 19:23:47 +0530

    s_125

---
+++ src/s_125/solution.java
@@ -0,0 +1,30 @@
+package s_125;
+
+
+ //* Definition for singly-linked list.
+  class ListNode {
+      int val;
+      ListNode next;
+      ListNode(int x) {
+          val = x;
+          next = null;
+      }
+  }
+
+public class solution {
+    public boolean hasCycle(ListNode head) {
+        ListNode slow=head;
+        ListNode fast=head;
+        while(fast!=null && fast.next!=null)
+        {
+            slow=slow.next;
+            fast=fast.next.next;
+            if(slow==fast)
+            {
+                return true;
+            }
+        }
+        return false;
+
+    }
+}
