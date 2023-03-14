283572845b1bf6e240786fbcec89b70106c63e8a
Deepanshu <semwaldeepanshu@gmail.com>
2023-03-14 15:46:27 +0530

    s_136

---
+++ src/s_136/solution.java
@@ -0,0 +1,43 @@
+package s_136;
+
+ class Node {
+    int data;
+    Node next;
+    Node(int x)
+    {
+        data=x;
+        next=null;
+    }
+}
+public class solution {
+     //  effcient approach
+    public void zigzag(Node head)
+    {
+        Node cur=head;
+        boolean flag=true;
+
+        while(cur!=null && cur.next!=null)
+        {
+            if(flag==true)
+            {
+                if(cur.data<cur.next.data)
+                {
+                    int temp=cur.data;
+                    cur.data=cur.next.data;
+                    cur.next.data=temp;
+                }
+            }
+            else
+            {
+                if(cur.data>cur.next.data)
+                {
+                    int temp=cur.data;
+                    cur.data=cur.next.data;
+                    cur.next.data=temp;
+                }
+            }
+            cur=cur.next;
+            flag=!(flag); //changing flag
+        }
+    }
+}
