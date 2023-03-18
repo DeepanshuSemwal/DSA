60cfad40854058ae2df4b6eaae32acf289ca06e9
Deepanshu <semwaldeepanshu@gmail.com>
2023-03-18 21:48:42 +0530

    18

---
+++ src/comibination_keypad/solution.java
@@ -0,0 +1,33 @@
+package comibination_keypad;
+
+public class solution {
+
+    public static String[] keypad={".","abc","def","ghi","jkl","mno","pqrs","tu","vwx","yz"};
+
+    public static void printCombination(String str,String combination,int n,int i)
+    {
+        if(i==n)
+        {
+            System.out.println(combination);
+            return;
+        }
+        char cur_char=str.charAt(i);
+        String mapping=keypad[cur_char-'0'];
+        for(int j=0;i< mapping.length();j++)
+        {
+            printCombination(str,mapping.charAt(j)+combination,n,i+1);
+
+        }
+
+    }
+
+    public static void main(String[] args) {
+        String str="23";
+        String combination="";
+        int i=0;
+        int n=str.length();
+        printCombination(str,combination,n,i);
+
+    }
+
+}
