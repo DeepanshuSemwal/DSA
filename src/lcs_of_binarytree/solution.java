97109c03ca8d130dfee3b78af8513f78f6b76cec
Deepanshu <semwaldeepanshu@gmail.com>
2023-03-20 23:55:29 +0530

    20

---
+++ src/lcs_of_binarytree/solution.java
@@ -0,0 +1,38 @@
+package lcs_of_binarytree;
+
+class Node
+{
+    Node left;
+    Node right;
+    int key;
+    Node(int k)
+    {
+        key=k;
+    }
+}
+public class solution {
+
+    public Node lcs(Node root,Node p,Node q)
+    {
+        if(root==null || p==null || q==null)
+        {
+            return root;
+        }
+        Node left=lcs(root.left,p,q);
+        Node right=lcs(root.right,p,q);
+        // result
+        if(root.left==null)
+        {
+            return right;
+        }
+        if(root.right==null)
+        {
+            return left;
+        }
+        else
+        {
+            // both left and right are not null then we get the given result
+            return root;
+        }
+    }
+}
