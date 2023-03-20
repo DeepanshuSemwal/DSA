d4b50de7b86d680a0ba2c26de264018e393de8f9
Deepanshu <semwaldeepanshu@gmail.com>
2023-03-20 14:03:16 +0530

    20

---
+++ src/check_for_balance/soultion.java
@@ -0,0 +1,41 @@
+package check_for_balance;
+
+class Node
+{
+    Node left;
+    Node right;
+    int key;
+    Node(int k)
+    {
+        key=k;
+    }
+}
+public class soultion {
+
+    public int check_balance(Node root)
+    {
+        if(root==null)
+        {
+            return 0;
+        }
+        int lh=check_balance(root.left);
+        if(lh==-1)
+        {
+
+        }
+        int rh=check_balance(root.right);
+        if(rh==-1)
+        {
+            return -1;
+        }
+        if(Math.abs(lh-rh)>1)
+        {
+            return -1;
+        }
+        else
+        {
+            // return height
+            return Math.max(lh,rh)+1;
+        }
+    }
+}
