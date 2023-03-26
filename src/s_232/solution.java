19c06fe22ff27034e7cb9099923fcd197aa6e51f
Deepanshu <semwaldeepanshu@gmail.com>
2023-03-26 21:51:13 +0530

    s_232

---
+++ src/s_232/solution.java
@@ -0,0 +1,54 @@
+package s_232;
+
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+
+class Node {
+    int data;
+    Node left, right;
+
+    public Node(int data){
+        this.data = data;
+    }
+}
+
+public class solution {
+
+    HashMap<String,Integer>map=new HashMap<>();
+    public List<Node> printAllDups(Node root)
+    {
+        //code here
+        ArrayList<Node>al=new ArrayList<>();
+        helper(root,al);
+        Collections.sort(al, (node1, node2) -> node1.data-node2.data); // we use this to arrange data in ascending order
+        return al;
+
+    }
+    public String helper(Node root,ArrayList<Node>al)
+    {
+        if(root==null)
+        {
+            return "$";
+        }
+        String subPath=helper(root.left,al)+","+root.data+","+helper(root.right,al);
+        if(map.containsKey(subPath))
+        {
+            map.put(subPath,map.get(subPath)+1);
+
+        }
+        else
+        {
+            map.put(subPath,1);
+        }
+        if(map.get(subPath)==2)
+        {
+            al.add(root);
+        }
+        return subPath;
+
+    }
+
+}
