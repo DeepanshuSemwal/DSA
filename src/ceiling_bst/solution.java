72c7e0f5250f77d787826151b8c1b624f3a1f87d
Deepanshu <semwaldeepanshu@gmail.com>
2023-04-04 00:15:20 +0530

    ceil_bst

---
+++ src/ceiling_bst/solution.java
@@ -0,0 +1,35 @@
+package ceiling_bst;
+
+class Node
+{
+    Node left;
+    Node right;
+    int data;
+    Node(int d)
+    {
+        data=d;
+    }
+}
+public class solution {
+    public static Node ceil(Node root,int x)
+    {
+        Node res=null;
+        while (root!=null)
+        {
+            if(x==root.data)
+            {
+                return root;
+            }
+            if(root.data>x)
+            {
+                res=root;
+                root=root.left;
+            }
+            else if(root.data<x)
+            {
+                root=root.right;
+            }
+        }
+        return res;
+    }
+}
