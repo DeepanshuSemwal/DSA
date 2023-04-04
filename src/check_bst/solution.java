bc4c623d1d6db27dfefe3bfdc5f2b52b293d87e3
Deepanshu <semwaldeepanshu@gmail.com>
2023-04-04 18:58:47 +0530

    checkBST

--- src/check_bst/solution.java
+++ src/check_bst/solution.java
@@ -12,13 +12,19 @@ class Node
 }
 public class solution {
 
+    // constraints ka dhiyan rakhna agar bade constraints diye hai toh phir Long.MAX_VALUE and LONG.MIN_VALUE in max and min
     public boolean isBST(Node root,int min,int max)
     {
       if(root==null)
       {
           return true;
       }
-      return (root.data>min && root.data<max && isBST(root.left,min,root.data) && isBST(root.right,root.data,max));
+      if(root.data<min || root.data>max)
+      {
+          return false;
+      }
+      return isBST(root.left,min,root.data) && isBST(root.right,root.data,max);
+
     }
 
 }
