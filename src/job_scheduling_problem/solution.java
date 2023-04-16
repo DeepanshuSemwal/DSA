e4a778b135396103167432b2b1b0bf6918b6ffab
Deepanshu <semwaldeepanshu@gmail.com>
2023-04-16 06:08:15 +0530

    job_scheduling

---
+++ src/job_scheduling_problem/solution.java
@@ -0,0 +1,50 @@
+package job_scheduling_problem;
+
+
+import java.util.Arrays;
+import java.util.Comparator;
+
+class Job {
+    int id, profit, deadline;
+    Job(int x, int y, int z){
+        this.id = x;
+        this.deadline = y;
+        this.profit = z;
+    }
+}
+
+public class solution {
+
+    int[] JobScheduling(Job arr[], int n)
+    {
+        // Your code here
+        // sort the array in decreasing order according to profit
+        int res[]=new int[2];
+        boolean slot[]=new boolean[n];
+        Arrays.sort(arr,(a,b)->b.profit-a.profit);
+        int job_id=0;
+        int profit=0;
+        for(Job job:arr)
+        {
+            int deadline=job.deadline;
+            while (deadline>=1)
+            {
+                if(slot[deadline-1]==false)
+                {
+                    slot[deadline-1]=true;
+                    profit+=job.profit;
+                    job_id++;
+                    break;
+                }
+                deadline--;
+            }
+        }
+        res[0]=job_id;
+        res[1]=profit;
+        return res;
+
+
+
+
+    }
+}
