a0bc8fa1329b06452b7d68f9e1c01345dedd6418
Deepanshu <semwaldeepanshu@gmail.com>
2023-04-26 15:50:34 +0530

    s_294

---
+++ src/s_294/solution.java
@@ -0,0 +1,34 @@
+package s_294;
+
+import java.util.ArrayList;
+import java.util.OptionalInt;
+
+public class solution {
+
+    public ArrayList<Integer> dfsOfGraph(int V, ArrayList<ArrayList<Integer>> adj) {
+        // Code here
+
+        ArrayList<Integer>al=new ArrayList<>();
+        boolean[]visited=new boolean[V];
+        for(int i=0;i<V;i++)
+        {
+            dfsRec(adj,visited,i,al);
+        }
+        return al;
+
+    }
+
+    public void dfsRec(ArrayList<ArrayList<Integer>> adj, boolean []visited, int s, ArrayList<Integer>al)
+    {
+        visited[s]=true;
+        al.add(s);
+        for(int v:adj.get(s))
+        {
+            if(visited[v]==false)
+            {
+                dfsRec(adj,visited,v,al);
+            }
+        }
+        return;
+    }
+}
