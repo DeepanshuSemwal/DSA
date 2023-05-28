ac34fc6e7daab4ba5c19a63640c524e084b021c8
Deepanshu <semwaldeepanshu@gmail.com>
2023-05-28 16:03:47 +0530

    28_may

--- src/s_278/solution.java
+++ src/s_278/solution.java
@@ -1,5 +1,60 @@
 package s_278;
 
+import java.sql.ClientInfoStatus;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
 public class solution {
+
+    public static ArrayList<ArrayList<String>>groupShifting(String[]strings)
+    {
+
+        HashMap<String,ArrayList<String>>map=new HashMap<>();
+        for(String str:strings)
+        {
+            String key=getKey(str);
+            if(!map.containsKey(key))
+            {
+                ArrayList<String>list=new ArrayList<>();
+                list.add(str);
+                map.put(key,list);
+            }
+            else
+            {
+                ArrayList<String>list=map.get(key);
+                list.add(str);
+            }
+        }
+        ArrayList<ArrayList<String>>res=new ArrayList<>();
+        for(ArrayList<String>al:map.values())
+        {
+            res.add(al);
+        }
+        return res;
+
+
+    }
+
+    public static String getKey(String str)
+    {
+        String key="";
+        for(int i=1;i<str.length();i++)
+        {
+            char first=str.charAt(i-1);
+            char second=str.charAt(i);
+            int diff=first-second;
+            // if it goes negative then we are adding 26
+            if(diff<0)
+            {
+                diff+=26;
+            }
+            key+=diff+"#";
+        }
+        key+=".";
+        return key;
+    }
+
 
 }
---
+++ src/s_282/solution.java
@@ -0,0 +1,71 @@
+package s_282;
+
+import java.util.Arrays;
+import java.util.HashSet;
+
+public class solution {
+
+    // using sorting
+    static int missingNumber1(int arr[], int size)
+    {
+        // Your code here
+        Arrays.sort(arr);
+        int res=1;
+        for(int i=0;i<size;i++)
+        {
+            if(arr[i]==res)
+            {
+                res++;
+            }
+        }
+        return res;
+
+    }
+
+    // using hashSet
+    HashSet<Integer>set=new HashSet<>();
+    static int missingNumber2(int arr[], int size)
+    {
+        HashSet<Integer>set=new HashSet<>();
+        for(int i=0;i<size;i++)
+        {
+            if(arr[i]>0)
+            {
+                set.add(arr[i]);
+            }
+        }
+        for(int i=0;i<size;i++)
+        {
+            if(!set.contains(i+1))
+            {
+                return i+1;
+            }
+        }
+        return size+1; // 1 to n positive elements are present then we know we need to return next element that is n+1
+    }
+
+    static int missingNumber(int arr[], int size)
+    {
+        for(int i=0;i<size;i++)
+        {
+            int cur_index=arr[i]-1;
+            while(arr[i]>0 && arr[i]<=size && arr[i]!=cur_index)
+            {
+                int temp=arr[i];
+                arr[i]=arr[cur_index];
+                arr[cur_index]=temp;
+                cur_index=arr[i]-1;
+            }
+        }
+        for(int i=0;i<size;i++)
+        {
+            if(arr[i]!=i+1)
+            {
+                return i+1;
+            }
+        }
+        return size+1;
+    }
+
+
+}
